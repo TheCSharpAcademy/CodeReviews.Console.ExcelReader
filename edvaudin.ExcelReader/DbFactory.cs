198b7950e95e9d40e124c60f27c3adb31c1cefa8
Edward Vaudin <edward@vaudin.net>
2023-02-11 20:25:30 +0000

    Fixes in line with Codacy analysis

--- edvaudin.ExcelReader/DbFactory.cs
+++ edvaudin.ExcelReader/DbFactory.cs
@@ -1,109 +1,115 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Text;
 
-namespace edvaudin.ExcelReader
+namespace edvaudin.ExcelReader;
+
+internal class DbFactory
 {
-    internal class DbFactory
+    private readonly string connectionString = ConfigurationManager.ConnectionStrings["exceldb"].ConnectionString;
+    public void CreateTable(DataTable table)
     {
-        private readonly string connectionString = ConfigurationManager.ConnectionStrings["exceldb"].ConnectionString;
-        public void CreateTable(DataTable table)
+        using var conn = new SqlConnection(connectionString);
+        conn.Open();
+        string sql = GetCreateTableQuery(table);
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+    }
+
+    public void DropExistingDataBase()
+    {
+        using var conn = new SqlConnection(connectionString);
+        conn.Open();
+        string sql = "DROP DATABASE exceldb";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+    }
+
+    public void CreateNewDatabase()
+    {
+        using var conn = new SqlConnection(connectionString);
+        conn.Open();
+        string sql = "CREATE DATABASE exceldb";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+    }
+
+    public void DisplayTable()
+    {
+        DataTable table = new();
+        using var conn = new SqlConnection(connectionString);
+        string sql = "SELECT * FROM exceldb.dbo.excel;";
+        SqlCommand cmd = new(sql, conn);
+        conn.Open();
+        using (var reader = cmd.ExecuteReader())
         {
-            using var conn = new SqlConnection(connectionString);
-            conn.Open();
-            string sql = GetCreateTableQuery(table);
-            SqlCommand cmd = new(sql, conn);
-            cmd.ExecuteNonQuery();
+            table.Load(reader);
         }
 
-        public void DropExistingDataBase()
-        {
-            using var conn = new SqlConnection(connectionString);
-            conn.Open();
-            string sql = "DROP DATABASE exceldb";
-            SqlCommand cmd = new(sql, conn);
-            cmd.ExecuteNonQuery();
-        }
+        DisplayData(table);
+    }
 
-        public void CreateNewDatabase()
-        {
-            using var conn = new SqlConnection(connectionString);
-            conn.Open();
-            string sql = "CREATE DATABASE exceldb";
-            SqlCommand cmd = new(sql, conn);
-            cmd.ExecuteNonQuery();
-        }
+    public void InsertIntoTable(DataTable table)
+    {
+        using var conn = new SqlConnection(connectionString);
+        conn.Open();
 
-        public void DisplayTable()
+        using SqlBulkCopy bulkCopy = new(conn);
+        bulkCopy.DestinationTableName = "exceldb.dbo.excel";
+
+        try
         {
-            DataTable table = new();
-            using var conn = new SqlConnection(connectionString);
-            string sql = "SELECT * FROM exceldb.dbo.excel;";
-            SqlCommand cmd = new(sql, conn);
-            conn.Open();
-            using (var reader = cmd.ExecuteReader())
+            bulkCopy.WriteToServer(table);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static void DisplayData(System.Data.DataTable table)
+    {
+        foreach (System.Data.DataRow row in table.Rows)
+        {
+            foreach (System.Data.DataColumn col in table.Columns)
             {
-                table.Load(reader);
+                Console.WriteLine("{0} = {1}", col.ColumnName, row[col]);
             }
-
-            DisplayData(table);
+            Console.WriteLine("============================");
         }
+    }
 
-        public void InsertIntoTable(DataTable table)
+    private static string GetCreateTableQuery(DataTable table)
+    {
+        StringBuilder stringBuilder = new();
+        stringBuilder.Append("CREATE TABLE exceldb.dbo.excel (");
+        for (int i = 0; i < table.Columns.Count; i++)
         {
-            using var conn = new SqlConnection(connectionString);
-            conn.Open();
-
-            using SqlBulkCopy bulkCopy = new(conn);
-            bulkCopy.DestinationTableName = "exceldb.dbo.excel";
-
-            try
+            stringBuilder.Append("\n [" + table.Columns[i].ColumnName + "] ");
+            string columnType = table.Columns[i].DataType.ToString();
+            switch (columnType)
             {
-                bulkCopy.WriteToServer(table);
+                case "System.Int32":
+                    stringBuilder.Append(" int ");
+                    break;
+                case "System.String":
+                default:
+                    stringBuilder.Append(string.Format(" nvarchar({0}) ", table.Columns[i].MaxLength == -1 ? "max" : table.Columns[i].MaxLength.ToString()));
+                    break;
             }
-            catch (Exception ex)
+            if (table.Columns[i].AutoIncrement)
             {
-                Console.WriteLine(ex.Message);
+                stringBuilder.Append(" IDENTITY(" + table.Columns[i].AutoIncrementSeed.ToString() + "," + table.Columns[i].AutoIncrementStep.ToString() + ") ");
+
             }
+            if (!table.Columns[i].AllowDBNull)
+            {
+                stringBuilder.Append(" NOT NULL ");
+            }
+            stringBuilder.Append(',');
         }
 
-        private static void DisplayData(System.Data.DataTable table)
-        {
-            foreach (System.Data.DataRow row in table.Rows)
-            {
-                foreach (System.Data.DataColumn col in table.Columns)
-                {
-                    Console.WriteLine("{0} = {1}", col.ColumnName, row[col]);
-                }
-                Console.WriteLine("============================");
-            }
-        }
-
-        private static string GetCreateTableQuery(DataTable table)
-        {
-            string sqlsc;
-            sqlsc = "CREATE TABLE exceldb.dbo.excel (";
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                sqlsc += "\n [" + table.Columns[i].ColumnName + "] ";
-                string columnType = table.Columns[i].DataType.ToString();
-                switch (columnType)
-                {
-                    case "System.Int32":
-                        sqlsc += " int ";
-                        break;
-                    case "System.String":
-                    default:
-                        sqlsc += string.Format(" nvarchar({0}) ", table.Columns[i].MaxLength == -1 ? "max" : table.Columns[i].MaxLength.ToString());
-                        break;
-                }
-                if (table.Columns[i].AutoIncrement)
-                    sqlsc += " IDENTITY(" + table.Columns[i].AutoIncrementSeed.ToString() + "," + table.Columns[i].AutoIncrementStep.ToString() + ") ";
-                if (!table.Columns[i].AllowDBNull)
-                    sqlsc += " NOT NULL ";
-                sqlsc += ",";
-            }
-            return sqlsc.Substring(0, sqlsc.Length - 1) + "\n)";
-        }
+        return stringBuilder.ToString()[..(stringBuilder.Length - 1)] + "\n)";
     }
 }
--- edvaudin.ExcelReader/Program.cs
+++ edvaudin.ExcelReader/Program.cs
@@ -1,32 +1,31 @@
 ﻿using OfficeOpenXml;
 
-namespace edvaudin.ExcelReader
+namespace edvaudin.ExcelReader;
+
+internal static class Program
 {
-    internal class Program
+    private static readonly DbFactory dbFactory = new();
+    static async Task Main()
     {
-        private static readonly DbFactory dbFactory = new();
-        static async Task Main(string[] args)
-        {
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            var file = new FileInfo("Demo.xlsx");
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        var file = new FileInfo("Demo.xlsx");
 
-            Console.WriteLine("Loading excel file into DataTable from: " + file.FullName);
-            var table = await SpreadsheetLoader.LoadExcelFile(file);
+        Console.WriteLine("Loading excel file into DataTable from: " + file.FullName);
+        var table = await SpreadsheetLoader.LoadExcelFile(file);
 
-            Console.WriteLine("Deleting pre-existing database...");
-            dbFactory.DropExistingDataBase();
+        Console.WriteLine("Deleting pre-existing database...");
+        dbFactory.DropExistingDataBase();
 
-            Console.WriteLine("Creating new database...");
-            dbFactory.CreateNewDatabase();
+        Console.WriteLine("Creating new database...");
+        dbFactory.CreateNewDatabase();
 
-            Console.WriteLine("Transferring DataTable structure to database...");
-            dbFactory.CreateTable(table);
+        Console.WriteLine("Transferring DataTable structure to database...");
+        dbFactory.CreateTable(table);
 
-            Console.WriteLine("Transferring DataTable contents to database...");
-            dbFactory.InsertIntoTable(table);
+        Console.WriteLine("Transferring DataTable contents to database...");
+        dbFactory.InsertIntoTable(table);
 
-            Console.WriteLine("Data transfer from Excel to SQL Server complete\n");
-            dbFactory.DisplayTable();
-        }
+        Console.WriteLine("Data transfer from Excel to SQL Server complete\n");
+        dbFactory.DisplayTable();
     }
 }
