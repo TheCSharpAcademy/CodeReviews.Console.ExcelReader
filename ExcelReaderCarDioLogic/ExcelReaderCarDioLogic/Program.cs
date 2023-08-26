b86b4eb5d47657dddb327762ea00cc10acb29ac3
João Silva <JPM_1993_Silva@hotmail.com>
2023-08-26 06:13:19 +0100

    Project Finished!

---
+++ ExcelReaderCarDioLogic/ExcelReaderCarDioLogic/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExcelReaderCarDioLogic;
+
+FileReaderLogic logic = new FileReaderLogic();
+DatabaseLogic model = new DatabaseLogic();
+Interface inter = new Interface();
+
+string filePath = inter.AskFilePath();
+(List<string> headers, List<List<string>> dataRows) = logic.ReadExcel(filePath);
+
+
+if (headers != null)
+{
+    inter.ShowData(headers, dataRows);
+
+    try
+    {
+        model.DeleteDatabase();
+    }
+    catch { }
+
+    model.CreateDatabase();
+
+    model.CreateTableAddData(headers, dataRows);
+}
