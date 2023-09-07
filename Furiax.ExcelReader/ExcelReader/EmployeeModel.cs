dbd299c6e57e5c35e14a2c78a14a63369df99616
furiax <carlmalfliet@proximus.be>
2023-09-07 14:33:47 +0200

    removed some data from xlsx file

--- Furiax.ExcelReader/ExcelReader/EmployeeModel.cs
+++ Furiax.ExcelReader/ExcelReader/EmployeeModel.cs
@@ -14,7 +14,7 @@ namespace ExcelReader
         public string Department { get; set; }
         public string Gender { get; set; }
         public string Ethnicity { get; set; }
-        public int Age { get; set; }
+        public string Age { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
     }
--- Furiax.ExcelReader/ExcelReader/Program.cs
+++ Furiax.ExcelReader/ExcelReader/Program.cs
@@ -2,20 +2,20 @@
 using OfficeOpenXml;
 using System.Drawing.Text;
 
-	ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-	//changed workingfolder to ExcelReader
-	string filePath = "EmployeeSampleData.xlsx";
-	var file = new FileInfo(filePath);	
+//changed workingfolder to ExcelReader
+string filePath = "EmployeeSampleData.xlsx";
+var file = new FileInfo(filePath);	
 
-	List<EmployeeModel> employeeList = await LoadExcelFile(file);
+List<EmployeeModel> employeeList = await LoadExcelFile(file);
 
 int counter = 1;
-	foreach (var employee in employeeList)
-	{
-        Console.WriteLine($"{counter}. {employee.EmployeeId} {employee.Name}");
+foreach (var employee in employeeList)
+{
+    Console.WriteLine($"{counter}. {employee.EmployeeId} {employee.Name}");
 	counter++;
-    }
+}
 
 static async Task<List<EmployeeModel>> LoadExcelFile(FileInfo file)
 {
@@ -36,10 +36,9 @@ static async Task<List<EmployeeModel>> LoadExcelFile(FileInfo file)
 		emp.Title = ws.Cells[row, col+2].Value.ToString();
 		emp.Department = ws.Cells[row, col+3].Value.ToString();
 		emp.Gender = ws.Cells[row, col+4].Value.ToString();
-		emp.Ethnicity = ws.Cells[row, col+5].Value.ToString();
-		emp.Age = int.Parse(ws.Cells[row, col+6].Value.ToString());
-		emp.Country = ws.Cells[row, col+7].Value.ToString();
-		emp.City = ws.Cells[row, col+8].Value.ToString();
+		emp.Age = ws.Cells[row, col+5].Value.ToString();
+		emp.Country = ws.Cells[row, col+6].Value.ToString();
+		emp.City = ws.Cells[row, col+7].Value.ToString();
 		output.Add(emp);
 		row++;
 	}
