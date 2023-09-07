624e66d427da6a5135a33d34a09ada9a9ed78dab
furiax <carlmalfliet@proximus.be>
2023-09-07 15:13:25 +0200

    added connectionstring

--- Furiax.ExcelReader/ExcelReader/ExcelReaderContext.cs
+++ Furiax.ExcelReader/ExcelReader/ExcelReaderContext.cs
@@ -10,5 +10,7 @@ namespace ExcelReader
 	internal class ExcelReaderContext : DbContext
 	{
 		public DbSet<EmployeeModel> Employees { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+			optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=ExcelReader;Trusted_Connection=True;");
 	}
 }
