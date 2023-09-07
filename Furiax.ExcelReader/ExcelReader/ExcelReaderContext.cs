d9e61f13f06630420e527de9c46bc57a737069f9
furiax <carlmalfliet@proximus.be>
2023-09-07 15:05:21 +0200

    Created the context page

---
+++ Furiax.ExcelReader/ExcelReader/ExcelReaderContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader
+{
+	internal class ExcelReaderContext : DbContext
+	{
+		public DbSet<EmployeeModel> Employees { get; set; }
+	}
+}
--- Furiax.ExcelReader/ExcelReader/Program.cs
+++ Furiax.ExcelReader/ExcelReader/Program.cs
@@ -3,14 +3,7 @@ using OfficeOpenXml;
 
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-//string filePath = "EmployeeSampleData.xlsx";
+//set the projectfolder as workingfolder so the application can find the xlsx file)
 var file = new FileInfo("EmployeeSampleData.xlsx");
 
 List<EmployeeModel> employeeList = await LoadExcel.LoadExcelFile(file);
-
-int counter = 1;
-foreach (var employee in employeeList)
-{
-	Console.WriteLine($"{counter}. {employee.EmployeeId} {employee.Name}");
-	counter++;
-}
