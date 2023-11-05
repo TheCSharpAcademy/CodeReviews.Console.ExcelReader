26f1a59c37e52563021dc473ff396ed6f3606aa4
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-11-05 15:03:01 +0100

    Add reading data from excel

--- ExcelReader.wkktoria/ExcelReader.wkktoria/Program.cs
+++ ExcelReader.wkktoria/ExcelReader.wkktoria/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using OfficeOpenXml;
 
-Console.WriteLine("Hello, World!");
+namespace ExcelReader.wkktoria;
+
+public static class Program
+{
+    public static void Main()
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+        Console.Write("Enter path to .xlsx file: ");
+        var filePath = Console.ReadLine();
+
+        if (!new FileInfo(filePath!).Exists)
+        {
+            Console.WriteLine("File doesn't exist!");
+            return;
+        }
+
+        var file = new FileInfo(filePath!);
+
+        using var package = new ExcelPackage(file);
+
+        var worksheet = package.Workbook.Worksheets[0];
+        var colCount = worksheet.Dimension.End.Column;
+        var rowCount = worksheet.Dimension.End.Row;
+
+        for (var row = 1; row < rowCount; row++)
+        for (var col = 1; col < colCount; col++)
+            Console.WriteLine($"Row: {row} Column: {col} Value: {worksheet.Cells[row, col].Value?.ToString()!.Trim()}");
+    }
+}
