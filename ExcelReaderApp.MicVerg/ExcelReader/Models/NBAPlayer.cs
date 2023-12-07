08b41c959ca64562d1795c9c42d031701cc93943
Michiel <michiel.vergauwen@visma.com>
2023-12-07 21:03:13 +0100

    Initial commit and finish :x

---
+++ ExcelReaderApp.MicVerg/ExcelReader/Models/NBAPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader.Models
+{
+    internal class NBAPlayer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string playerName { get; set; }
+        public int gamesPlayed { get; set; }
+        public double pointsPerGame { get; set; }
+        public double minutesPerGame { get; set; }
+        public double reboundsPerGame { get; set; }
+        public double assistsPerGame { get; set; }
+    }
+}
---
+++ ExcelReaderApp.MicVerg/ExcelReader/Models/NBAPlayerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader.Models
+{
+    internal class NBAPlayerContext : DbContext
+    {
+        public DbSet<NBAPlayer> NBAPlayers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-ETA4JL7;Database=ExcelReader;Trusted_Connection=True;Integrated Security=True;Encrypt=False;");
+        }
+    }
+}
