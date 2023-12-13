5f10169f013ecffbd6f805e0b751ade679a9a9c4
K-MYR <144546255+K-MYR@users.noreply.github.com>
2023-12-13 11:32:08 +0100

    Added support for any number of columns

---
+++ ExcelReader.K-MYR/ExcelReader.K-MYR/DataReader.cs
@@ -0,0 +1,30 @@
+﻿using OfficeOpenXml;
+using System.Data;
+
+
+namespace ExcelReader.K_MYR;
+
+internal class DataReader
+{
+    public readonly SQLServerRepo Repo;
+
+    public DataReader(SQLServerRepo repo)
+    {
+        Repo = repo;
+    }
+
+    public DataTable? ReadFromXlsx(FileInfo file)
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+        var package = new ExcelPackage(file);
+
+        var ws = package.Workbook.Worksheets[0];
+
+        Repo.CreateDatabase();
+        Repo.CreateTable(ws);
+        var data = Repo.InsertDataWithOleDB(ws, file);
+
+        return data;
+    }
+}
---
+++ ExcelReader.K-MYR/ExcelReader.K-MYR/Program.cs
@@ -0,0 +1,7 @@
+﻿using ExcelReader.K_MYR;
+
+SQLServerRepo repo = new();
+DataReader reader = new(repo);
+UserInterface ui = new(reader);
+
+ui.ShowMainMenu();
