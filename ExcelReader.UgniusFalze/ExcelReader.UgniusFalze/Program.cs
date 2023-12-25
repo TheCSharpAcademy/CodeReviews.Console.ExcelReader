329ed9a6a088f701de02e940f1a1e096b07fb2bc
ugnius <ugnius.vilkoncius@gmail.com>
2023-12-26 00:44:47 +0200

    Finished

--- ExcelReader.UgniusFalze/ExcelReader.UgniusFalze/Program.cs
+++ ExcelReader.UgniusFalze/ExcelReader.UgniusFalze/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ExcelReader.UgniusFalze.Services;
 
-Console.WriteLine("Hello, World!");
+try
+{
+    Display.DisplayStart();
+    var driver = new Driver(@"Server=(LocalDb)\MSSQLLocalDB;Integrated Security=SSPI;Trusted_Connection=yes;");
+    Display.DisplayDatabaseDrop();
+    driver.DropDatabase();
+    Display.DisplayDatabaseCreate();
+    driver.CreateDatabase();
+    Display.DisplayTableCreate();
+    driver.CreateTable();
+    Display.DisplayReadingFile();
+    using var fileReader = new FileReader();
+    var columns = fileReader.GetColumns();
+    if (fileReader.GetColumns() == null)
+    {
+        Display.DisplayNotFound();
+        return;
+    }
+    var planesFromExcel = fileReader.GetData();
+    Display.DisplayWritingToDatabase();
+    foreach (var planeInExcel in planesFromExcel)
+    {
+        driver.InsertPlane(planeInExcel);
+    }
+    var planes = driver.GetPlanes();
+    Display.DisplayData(planes, columns);
+    Display.DisplayDone();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
