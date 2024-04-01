2a396b2d18a64260be58aa4d0396c8b5a8d34d57
Diego Arroyo <diego.arroyo2462050@gmail.com>
2024-04-01 08:49:47 -0300

    ExcelReader.Lonchanick

---
+++ ExcelReader.Lonchanick/ContextDB.cs
@@ -0,0 +1,18 @@
+﻿using exelReader2._0.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exelReader2._0;
+
+internal class ContextDB : DbContext
+{ 
+
+    public DbSet<Person> Persons { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { 
+        optionsBuilder.UseSqlite($"Data Source =  Persons.db");
+
+    }
+
+
+
+}
