b9e5d548f19cb37102df580b443471035ca78c40
Youngwoo Jin <duddnwkfgo7@gmail.com>
2024-05-19 16:37:48 -0700

    re-commit after deleting unnecessary files

--- ExcelReader.Bata13/STUDY.ConsoleP.ExcelReader/STUDY.ConsoleP.ExcelReader/STUDY.ConsoleP.ExcelReader/Models/ExcelDataModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace STUDY.ConsoleP.ExcelReader.Models;
-internal class ExcelDataModel
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
