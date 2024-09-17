47a5fa9169e3c6cdc5ada89895634e834277b3c4
N-Endy <okafornel@gmail.com>
2024-09-17 22:45:39 +0100

    Optimised Program.cs

--- ExcelReader.UndercoverDev/ExcelReader/Program.cs
+++ ExcelReader.UndercoverDev/ExcelReader/Program.cs
@@ -1,4 +1,33 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Extensions.Hosting;
+// using ExcelReader.Controllers;
+// using ExcelReader.Services;
+// using ExcelReader.Utilities;
+// using ExcelReader.Data;
+// using Microsoft.Extensions.Configuration;
+
+// HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+// builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+// builder.Services.AddSingleton<DatabaseController>();
+// builder.Services.AddSingleton<ExcelReaderController>();
+// builder.Services.AddSingleton<DatabaseService>();
+// builder.Services.AddSingleton<ExcelService>();
+// builder.Services.AddSingleton<Logger>();
+// builder.Services.AddDbContext<AppDbContext>();
+
+// var app = builder.Build();
+
+// var scope = app.Services.CreateScope();
+// var servicesExtension = scope.ServiceProvider;
+
+// var controller = servicesExtension.GetRequiredService<DatabaseController>();
+// controller.InitializeDatabase();
+// controller.DisplayData();
+
+
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ExcelReader.Controllers;
 using ExcelReader.Services;
@@ -6,22 +35,30 @@ using ExcelReader.Utilities;
 using ExcelReader.Data;
 using Microsoft.Extensions.Configuration;
 
-HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+var builder = Host.CreateApplicationBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-builder.Services.AddSingleton<DatabaseController>();
-builder.Services.AddSingleton<ExcelReaderController>();
-builder.Services.AddSingleton<DatabaseService>();
-builder.Services.AddSingleton<ExcelService>();
-builder.Services.AddSingleton<Logger>();
-builder.Services.AddDbContext<AppDbContext>();
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-var scope = app.Services.CreateScope();
-var servicesExtension = scope.ServiceProvider;
+InitializeDatabase(app.Services);
 
-var controller = servicesExtension.GetRequiredService<DatabaseController>();
-controller.InitializeDatabase();
-controller.DisplayData();
+// await app.RunAsync();
+
+void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddSingleton<DatabaseController>();
+    services.AddSingleton<ExcelReaderController>();
+    services.AddSingleton<DatabaseService>();
+    services.AddSingleton<ExcelService>();
+    services.AddSingleton<Logger>();
+    services.AddDbContext<AppDbContext>();
+}
+
+void InitializeDatabase(IServiceProvider serviceProvider)
+{
+    using var scope = serviceProvider.CreateScope();
+    var controller = scope.ServiceProvider.GetRequiredService<DatabaseController>();
+    controller.InitializeDatabase();
+    controller.DisplayData();
+}
