584877444eaaacc9b7aa077861bd30ab1fa3dc48
N-Endy <okafornel@gmail.com>
2024-09-17 20:41:18 +0100

    Add code for Excel Service

--- ExcelReader.UndercoverDev/ExcelReader/Services/ExcelService.cs
+++ ExcelReader.UndercoverDev/ExcelReader/Services/ExcelService.cs
@@ -1,10 +1,34 @@
 using ExcelReader.Models;
+using OfficeOpenXml;
 
 namespace ExcelReader.Services;
 public class ExcelService
 {
     internal List<DataModel> ReadExcel()
     {
-        throw new NotImplementedException();
+        var filePath = "../Resources/matches.xlsx";
+        var data = new List<DataModel>();
+
+        // Read data from Excel file and populate the data list
+        using var package = new ExcelPackage(new FileInfo(filePath));
+
+        var workSheet = package.Workbook.Worksheets[0];
+        var rowCount = workSheet.Dimension.Rows;
+
+        for (var row = 2; row <= rowCount; row++)
+        {
+            var dataModel = new DataModel
+            {
+                Date = workSheet.Cells[row, 1].Value?.ToString() ?? string.Empty,
+                League = workSheet.Cells[row, 2].Value?.ToString() ?? string.Empty,
+                Home = workSheet.Cells[row, 3].Value?.ToString() ?? string.Empty,
+                Away = workSheet.Cells[row, 4].Value?.ToString() ?? string.Empty,
+                HomeProbability = workSheet.Cells[row, 5].Value?.ToString() ?? string.Empty,
+                AwayProbability = workSheet.Cells[row, 6].Value?.ToString() ?? string.Empty,
+                OverTwoGoals = workSheet.Cells[row, 7].Value?.ToString() ?? string.Empty
+            };
+            data.Add(dataModel);
+        }
+        return data;
     }
 }
