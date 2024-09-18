340cb3b0ccb9d220eb53dd8424a6be8eabc55a8b
N-Endy <okafornel@gmail.com>
2024-09-18 10:12:38 +0100

    Refractor Program file

--- ExcelReader.UndercoverDev/ExcelReader/Program.cs
+++ ExcelReader.UndercoverDev/ExcelReader/Program.cs
@@ -1,33 +1,4 @@
-﻿// using Microsoft.Extensions.DependencyInjection;
-// using Microsoft.Extensions.Hosting;
-// using ExcelReader.Controllers;
-// using ExcelReader.Services;
-// using ExcelReader.Utilities;
-// using ExcelReader.Data;
-// using Microsoft.Extensions.Configuration;
-
-// HostApplicationBuilder builder = Host.CreateApplicationBuilder();
-
-// builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-// builder.Services.AddSingleton<DatabaseController>();
-// builder.Services.AddSingleton<ExcelReaderController>();
-// builder.Services.AddSingleton<DatabaseService>();
-// builder.Services.AddSingleton<ExcelService>();
-// builder.Services.AddSingleton<Logger>();
-// builder.Services.AddDbContext<AppDbContext>();
-
-// var app = builder.Build();
-
-// var scope = app.Services.CreateScope();
-// var servicesExtension = scope.ServiceProvider;
-
-// var controller = servicesExtension.GetRequiredService<DatabaseController>();
-// controller.InitializeDatabase();
-// controller.DisplayData();
-
-
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ExcelReader.Controllers;
 using ExcelReader.Services;
@@ -59,6 +30,5 @@ void InitializeDatabase(IServiceProvider serviceProvider)
 {
     using var scope = serviceProvider.CreateScope();
     var controller = scope.ServiceProvider.GetRequiredService<DatabaseController>();
-    controller.InitializeDatabase();
-    controller.DisplayData();
+    controller.RunApp();
 }
