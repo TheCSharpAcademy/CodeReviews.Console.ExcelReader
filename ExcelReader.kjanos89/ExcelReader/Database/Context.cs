17da057f5e7bb90020740017679f15346d1f691a
kjanos89 <91398032+kjanos89@users.noreply.github.com>
2024-09-22 10:50:10 +0200

    Finished the project (#42)
    * Clone and basic setup
    
    -Cloned the project
    -Created the minimum necessary classes
    -Added the basic data flow
    
    * Final touches
    
    -Added the rest of the logic.
    -Everything is in try-catch blocks.
    -Note to self: Without user input everything is much simpler and easier to track.
    
    * Final-final touches
    
    -Removed a bunch of console.clear()s because in case of an error even the messages would be erased from the console without a chance of reading them.
    Now there's only one right before displaying the data.
    
    * Codacy fixes
    
    -Restructured service class to have an Initialize() method that can be called when right after instantiation of the class as an object.
    -Removed the using System.Configuration and added it to the code as a workaround. Let's see if it works.

---
+++ ExcelReader.kjanos89/ExcelReader/Database/Context.cs
@@ -0,0 +1,30 @@
+﻿using ExcelReader.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExcelReader.Database
+{
+    public class Context : DbContext
+    {
+        public Context() 
+        {
+            
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<ExcelData> ExcelDbSet { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Console.WriteLine("Configuring database...");
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ExcelReaderDB"]?.ConnectionString;
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
---
+++ ExcelReader.kjanos89/ExcelReader/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExcelReader.Service;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Loading, please wait...");
+        Service service = new Service();
+        service.Initialize();
+    }
+}
