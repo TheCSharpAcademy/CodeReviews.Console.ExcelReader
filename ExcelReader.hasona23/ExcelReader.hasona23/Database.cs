b7b8f8bef0bfb9b7808d4bef7cc3bf4e995e2ab8
hasona23 <130175104+hasona23@users.noreply.github.com>
2024-11-11 18:18:39 +0200

    Last commit

---
+++ ExcelReader.hasona23/ExcelReader.hasona23/Database.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace ExcelReader.hasona23;
+
+public class Database
+{
+    private readonly string _connectionString;
+    private readonly string _tableName;
+    private readonly ExcelFileHandler _excelFileHandler;
+
+    public Database(ExcelFileHandler excelFileHandler)
+    {
+        _connectionString = $"Data Source = {excelFileHandler.Worksheet.Name}.db;";
+        _tableName = $"BikeStore";
+        CreateTable();
+        _excelFileHandler = excelFileHandler;
+        List<string> colNames = [];
+        for (int col = 1; col <= excelFileHandler.ColumnCount; col++)
+        {
+            colNames.Add(excelFileHandler.Worksheet.Cells[1, col].Text);
+        }
+
+        Console.WriteLine($"Table {_tableName} has been created");
+        AddTableColumns(colNames);
+    }
+
+    private void AddTableColumns(List<string> tableColumns)
+    {
+        using (var conn = new SqliteConnection(_connectionString))
+        {
+            Console.WriteLine("Adding Columns...");
+            conn.Open();
+            using (var command = conn.CreateCommand())
+            {
+                try
+                {
+                    foreach (var column in tableColumns)
+                    {
+                        string addColumn = $"ALTER TABLE {_tableName} ADD {column} TEXT";
+                        command.CommandText = addColumn;
+                        command.ExecuteNonQuery();
+                        Console.WriteLine($"Added {column}...");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            conn.Close();
+        }
+    }
+
+    private void CreateTable()
+    {
+        string deletionCommand = $"DROP TABLE IF EXISTS  {_tableName};";
+        string creationCommand = $@"CREATE TABLE {_tableName} ([ID] INTEGER  PRIMARY KEY AUTOINCREMENT )";
+        using (var conn = new SqliteConnection(_connectionString))
+        {
+            conn.Open();
+            Console.WriteLine("Cleaning Database...");
+            using (var command = new SqliteCommand(deletionCommand, conn))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            Console.WriteLine("Creating Database...");
+            using (var command = new SqliteCommand(creationCommand, conn))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            conn.Close();
+        }
+    }
+
+    public void TransferData()
+    {
+        Console.WriteLine("Transferring data...");
+        List<string> rowData = [];
+        using (var conn = new SqliteConnection(_connectionString))
+        {
+            conn.Open();
+            try
+            {
+                using var command = conn.CreateCommand();
+                for (int row = 2; row <= _excelFileHandler.RowCount; row++)
+                {
+                    for (int col = 1; col <= _excelFileHandler.ColumnCount; col++)
+                    {
+
+
+                        rowData.Add(_excelFileHandler.Worksheet.Cells[row, col].Text.Trim());
+
+                    }
+
+                    string cmdText =
+                        $@"INSERT INTO {_tableName} VALUES(NULL,'{rowData[0]}','{rowData[1]}','{rowData[2]}','{rowData[3]}','{rowData[4]}','{rowData[5]}','{rowData[6]}')";
+                    command.CommandText = cmdText;
+                    command.ExecuteNonQuery();
+                    rowData.Clear();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            conn.Close();
+        }
+    }
+
+    public List<List<string>> GetData()
+    {
+        Console.WriteLine("Getting Data from Database");
+        List<List<string>> data = [];
+        string getCommand = $"SELECT bike_id,brand,model,price,color,size,weight FROM  {_tableName}";
+        using (var conn = new SqliteConnection(_connectionString))
+        {
+            conn.Open();
+            try
+            {
+                using (var command = conn.CreateCommand())
+                {
+                    command.CommandText = getCommand;
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        data.Add([
+                            reader.GetString(0), reader.GetString(1),
+                            reader.GetString(2), reader.GetString(3),
+                            reader.GetString(4), reader.GetString(5),
+                            reader.GetString(6)
+                        ]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            conn.Close();
+        }
+
+        return data;
+    } 
+}
--- ExcelReader.hasona23/ExcelReader.hasona23/Program.cs
+++ ExcelReader.hasona23/ExcelReader.hasona23/Program.cs
@@ -1,2 +1,22 @@
-﻿Console.WriteLine("Hello World!");
+﻿using ExcelReader.hasona23;
+using Spectre.Console;
+using Spectre.Console.Rendering;
 
+ExcelFileHandler excel = new("BikeStore.xlsx","data");
+Database db = new Database(excel);
+db.TransferData();
+var data = db.GetData();
+Console.WriteLine("Displaying Table...");
+Table t = new Table().Title("BikeStore");
+t.Alignment(Justify.Center);
+t.Expand();
+t.BorderStyle(new Style(Color.Yellow));
+t.AddColumns("[darkturquoise]BikeId[/]","[darkturquoise]Brand[/]","[darkturquoise]Model[/]","[darkturquoise]Price[/]"
+    ,"[darkturquoise]Color[/]","[darkturquoise]Size[/]","[darkturquoise]Weight[/]");
+t.Border(new HeavyTableBorder());
+foreach (var row in data)
+{
+    t.AddRow($"[white]{row[0]}[/]",$"[white]{row[1]}[/]",$"[white]{row[2]}[/]",$"[white]{row[3]}[/]",$"[white]{row[4]}[/]",
+        $"[white]{row[5]}[/]",$"[white]{row[6]}[/]");
+}
+AnsiConsole.Write(t);
